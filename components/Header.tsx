fc64b683dfe4fc7fd8d0b3371f630e7f771715a2
junli7 <2007junli@gmail.com>
2025-10-09 18:07:05 -0400

    Hehe

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ const Header: React.FC<HeaderProps> = ({ onAddSchool, onOpenManageTags, onImport
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           <h1 className="text-2xl sm:text-3xl font-bold text-zinc-900 dark:text-white">
-            College Application Tracker
+            jun's app tracker
           </h1>
           <div className="flex items-center gap-2 sm:gap-4">
             <button
